919a6f10cc6c25f4ccfdf20b35921654691f368e
steven0323 <Steph199603@gmail.com>
2022-11-26 14:48:24 -0800

    add anon-desc anonymization

--- build_cohort.sh
+++ build_cohort.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
-
-echo "Start anonymizing raw data ..."
-
 # Please specify patient_directory and the place you want to store info.csv before you run the script in default.yaml
-#patient_dir="../unanonymized_data/unanon_70/generalization_dataset/experiment1/all_data/raw/*/"
-#new_dir="../anonymized_data/anon_70/experiment1/all_data/raw"
-#info="../anonymized_data/anon_70/info.csv"
 
-patient_dir="${PATIENT_DIR:-$(yq ".patient_dir" default.yaml)}"
-new_dir="${NEW_DIR:-$(yq ".new_dir" default.yaml)}"
+raw_patient_dir="${RAW_PATIENT_DIR:-$(yq ".raw_patient_dir" default.yaml)}" 
+meta_patient_dir="${META_PATIENT_DIR:-$(yq ".meta_patient_dir" default.yaml)}" 
+new_dir="${NEW_DIR:-$(yq ".new_dir" default.yaml)}" 
 info="${INFO:-$(yq ".info" default.yaml)}"
+anon_desc="${ANON_DESC:-$(yq ".anon_desc" default.yaml)}"
 
+export raw_patient_dir meta_patient_dir new_dir info
 
+anonymize_rawdata(){
+    echo "Start anonymizing raw data ..."
+    for dir in ${raw_patient_dir}; do
+	echo "test"
+        # Preprocessing file from each patient directories and then anonymize data
+	python ventmap/validate_data_type.py $dir
+	python ventmap/anonymize_datatimes.py $dir --new-cohort-file=$info --new-dir=$new_dir
+    done
 
-for dir in ${patient_dir}; do
-    python ventmap/anonymize_datatimes.py $dir --new-cohort-file=$info --new-dir=$new_dir
-done
+    echo "Anonymization finished !!"
+}
 
-echo "Anonymization finished !!"
+update_cohort (){
+    echo "Update cohort description"
+    python $HOME/Desktop/deepards/deepards/redo_cohort_desc_after_anonymization.py --shift-file=$info --non-anon-cohort-desc=$anon_desc
+    echo "Update cohort description completed !"
+
+}
+
+anonymize_rawdata
+update_cohort
 
 
